e5ed8a817a8c07bea963716610b1fe1992fd4aaa
Sudeep <sudeepsudee828@gmail.com>
2026-06-02 18:11:42 +0530

    Harmonious

--- slidingwindow/index.js
+++ slidingwindow/index.js
@@ -11,6 +11,8 @@
 //   return maxsum
 // }
 
+const { count } = require("console");
+
 // let res = consum([2,1,5,1,3,2],3)
 // console.log((res));
 
@@ -42,5 +44,29 @@
 // let res = duplicate([1,2,3,1],3);
 // console.log(res)
 
-// Longest Harmonious Subsequence
+//Longest Harmonious Subsequence
+//we have Harmonious array
+//maximum  Value - minValue
+// the subarray need to be if u calculate the diffrence that needs to be 1
+// means 3-2 = 1
+// i can use o(n^2)
 
+function Harmonious(nums) {
+  let max = 0
+  for(let i=0;i<nums.length;i++){
+    let a = nums[i]
+  let count1=0,count2=0
+  for(let j=0;j<nums.length;j++){
+      if(nums[j] === a)
+    count1++;
+if(nums[j] === a + 1)
+    count2++;
+  }
+  if(count2 > 0){
+    max = Math.max(max, count1 + count2);
+}
+  }
+return max
+}
+let res = Harmonious([1, 3, 2, 2, 5, 2, 3, 7]);
+console.log(res);
