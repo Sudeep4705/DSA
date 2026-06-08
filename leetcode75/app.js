30b6e5602e7417ee0fb29d61001ccbae30528030
Sudeep <sudeepsudee828@gmail.com>
2026-06-08 18:45:36 +0530

    gcdStrings

--- leetcode75/app.js
+++ leetcode75/app.js
@@ -1,22 +1,60 @@
 
 
-function mergestr(word1,word2){
-let i=0,j=0,str=""
-    while(i<word1.length && j<word2.length){
-            str+=word1[i]+word2[j]
-            i++
-            j++
+// function mergestr(word1,word2){
+// let i=0,j=0,str=""
+//     while(i<word1.length && j<word2.length){
+//             str+=word1[i]+word2[j]
+//             i++
+//             j++
+//     }
+//     while(i<word1.length){
+//         str+=word1[i]
+//         i++
+//     }
+//     while(j<word2.length){
+//         str+=word2[j]
+//         j++
+//     }
+//     return str
+// }
+
+// let res = mergestr("ab","pqrs")
+// console.log(res);
+
+
+// function divisorString(str1,str2){
+
+//     let temp= ""
+//     for(let i=0;i<str2.length;i++){
+//             temp = str2[i]
+//         for(let j=0;j<str1.length;j++){
+//             temp+=temp
+//         }
+//         console.log(temp);
+        
+//     }
+// }
+
+// let res = divisorString("ABCABC","ABC")
+// console.log(res);
+
+function gcdOfStrings(str1, str2) {
+    function canBuild(str, pattern) {
+        let temp = "";
+        while (temp.length < str.length) {
+            temp += pattern;
+        }
+        return temp === str;
     }
-    while(i<word1.length){
-        str+=word1[i]
-        i++
+    let answer = "";
+    for (let i = 1; i <= Math.min(str1.length,str2.length); i++) {
+        let candidate = str1.slice(0, i);
+        console.log(candidate);
+        if (canBuild(str1, candidate) && canBuild(str2, candidate)) {
+            answer = candidate;
+        }
     }
-    while(j<word2.length){
-        str+=word2[j]
-        j++
-    }
-    return str
+    return answer;
 }
 
-let res = mergestr("ab","pqrs")
-console.log(res);
+console.log(gcdOfStrings("ABCABC", "ABC")); 
