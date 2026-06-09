312b8c2b37ab187a6d48a5046cc28ba3675aee97
Sudeep <sudeepsudee828@gmail.com>
2026-06-09 17:43:14 +0530

    candies

--- leetcode75/app.js
+++ leetcode75/app.js
@@ -38,23 +38,47 @@
 // let res = divisorString("ABCABC","ABC")
 // console.log(res);
 
-function gcdOfStrings(str1, str2) {
-    function canBuild(str, pattern) {
-        let temp = "";
-        while (temp.length < str.length) {
-            temp += pattern;
-        }
-        return temp === str;
+// function gcdOfStrings(str1, str2) {
+//     function canBuild(str,pattern) {
+//         let temp = "";
+//         while (temp.length < str.length) {
+//             temp += pattern;
+//         }
+//         return temp === str;
+//     }
+//     let answer = "";
+//     for (let i = 1; i <= Math.min(str1.length,str2.length); i++) {
+//         let candidate = str1.slice(0, i);
+//         console.log(candidate);
+//         if (canBuild(str1, candidate) && canBuild(str2, candidate)) {
+//             answer = candidate;
+//         }
+//     }
+//     return answer;
+// }
+
+// console.log(gcdOfStrings("ABCABC", "ABC")); 
+
+function candies(candies, extraCandies){
+let max = 0
+let arr = []
+for(let i=0;i<candies.length;i++){
+    if(max<candies[i]){
+        max=candies[i]
     }
-    let answer = "";
-    for (let i = 1; i <= Math.min(str1.length,str2.length); i++) {
-        let candidate = str1.slice(0, i);
-        console.log(candidate);
-        if (canBuild(str1, candidate) && canBuild(str2, candidate)) {
-            answer = candidate;
-        }
-    }
-    return answer;
 }
 
-console.log(gcdOfStrings("ABCABC", "ABC")); 
+for(let j=0;j<candies.length;j++){
+    let sum = 0
+    sum= candies[j] + extraCandies
+    if(sum>=max){
+        arr[j]=true
+    }else{
+        arr[j]=false
+    }
+}
+return arr
+}
+
+let res = candies([2,3,5,1,3],3)
+console.log(res);
