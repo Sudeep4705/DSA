dd3fd29e3663d1479fdde79e922bd6bb3f2fef80
Sudeep <sudeepsudee828@gmail.com>
2026-08-13 18:39:18 +0530

    hasdup and concat

--- Neetcode250/app.js
+++ Neetcode250/app.js
@@ -1,30 +1,3 @@
-// function oddNumbersLessThanTen() {
-//   let currentNumber = 1; //step 1
- 
-//   while (currentNumber < 10) { //step2
-//     if (currentNumber % 2 !== 0) { //step3
-//       console.log(currentNumber);//step4
-//     }
-
-//     currentNumber += 1;//step5
-//   }
-// }
-
-// oddNumbersLessThanTen()
-
-// let arr = [1, 2, 3, 4, 5];
-
-// function getConcatenation(nums){
-// let ans = new Array(nums.length*2)
-// for(let i =0;i<ans.length;i++){
- 
-// }
-// return ans
-// }
-
-// let res = getConcatenation([1,4,1,2])
-// console.log(res);
-
 // function twosum(nums,target){
 //   for(let i=0;i<nums.length;i++){
 //             for(let j=i+1;j<nums.length;j++){
@@ -38,21 +11,58 @@
 // console.log(res);
 
 
-function isAnagram(s,t){
-if(s.length!==t.length){
-    return false
-}
-let sorted_S = s.split("").sort().join("")
-let sorted_t = t.split("").sort().join("")
-if(sorted_S!==sorted_t){
-    return false
-}else{
-    return true
-}
+// function isAnagram(s,t){
+// if(s.length!==t.length){
+//     return false
+// }
+// let sorted_S = s.split("").sort().join("")
+// let sorted_t = t.split("").sort().join("")
+// if(sorted_S!==sorted_t){
+//     return false
+// }else{
+//     return true
+// }
 
-}
-let res = isAnagram("racecar","carrace")
-console.log(res);
+// }
+// let res = isAnagram("racecar","carrace")
+// console.log(res);
 
 
 
+// function getConcatenation(nums){
+// let ans = new Array(nums.length*2)
+// let copy = nums
+
+// let k=0;
+// while(k<ans.length){
+// for( let i=0;i<nums.length;i++){
+//     ans[k] = nums[i]
+//     k++
+// }
+
+// for(let j=0;j<copy.length;j++){
+//     ans[k]=copy[j]
+//     k++
+// }
+// }
+// return ans
+// }
+// let res = getConcatenation([1,4,1,2])
+// console.log(res);
+
+
+
+// function hasDuplicate(nums){
+// let newArr = []
+// for(let i=0;i<nums.length;i++){
+//     if(!newArr.includes(nums[i])){
+//         newArr.push(nums[i])
+//     }else{
+//         return true
+//     }
+// }
+// return false
+// }
+
+// let res = hasDuplicate([1, 2, 3, 4])
+// console.log(res);
