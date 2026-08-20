ecd3e9d3086dfb0ccf24b05a97d7b0a24fee2a81
Sudeep <sudeepsudee828@gmail.com>
2026-08-20 17:43:52 +0530

    lcp

--- Neetcode250/app.js
+++ Neetcode250/app.js
@@ -67,37 +67,41 @@
 // let res = hasDuplicate([1, 2, 3, 4])
 // console.log(res);
 
-// function longestCommonPrefix(strs){
-// strs = strs.join(" ")
-// for(let i=0;i<strs.length;i++){
-//     for(let j=0;j<strs.length;j++){
-        
-        
-//     }
-    
+
+// function  removeElement(nums,val){
+// let i=0
+// let j=0
+// while(i<nums.length){
+// if(nums[i]!=val){
+//     let temp = nums[i]
+//     nums[i]=nums[j]
+//     nums[j] = temp
+//     j++
 // }
-
-     
-
+// i++
 // }
-
-// let res = longestCommonPrefix(["bat","bag","bank","band"])
+// return nums.slice(0,j)
+// }
+// let res = removeElement([3,2,2,3],3)
 // console.log(res);
 
-function  removeElement(nums,val){
-let i=0
-let j=0
-while(i<nums.length){
-if(nums[i]!=val){
-    let temp = nums[i]
-    nums[i]=nums[j]
-    nums[j] = temp
-    j++
-}
-i++
-}
-return nums.slice(0,j)
-}
-let res = removeElement([3,2,2,3],3)
-console.log(res);
 
+function longestCommonPrefix(strs){
+let  prefix = strs[0]
+
+for(let i=1;i<strs.length;i++){
+    console.log("ele",strs[i]);
+    while(!strs[i].startsWith(prefix)){
+        prefix = prefix.slice(0,-1)
+        console.log(prefix);
+        
+        if(prefix==""){
+            return ""
+        }
+    }
+}
+return prefix
+}
+
+let res = longestCommonPrefix(["bat","bag","bank","band"])
+console.log(res);
