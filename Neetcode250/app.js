32432ea1ffb560ed4b5f0a5d926d6cc6679fb527
Sudeep <sudeepsudee828@gmail.com>
2026-08-29 17:47:20 +0530

    topkfrequent

--- Neetcode250/app.js
+++ Neetcode250/app.js
@@ -134,22 +134,73 @@
 // console.log(res);
 
 
-function majority(nums){
-let ans =  nums[0]
-let count =1 
-for(let i=1;i<nums.length;i++){
-    if(count==0){
-        ans = nums[i]
-        count=1
-    }else if(nums[i]===ans){
-        count++
-    }else{
-        count--
-    }
+// function majority(nums){
+// let ans =  nums[0]
+// let count =1 
+// for(let i=1;i<nums.length;i++){
+//     if(count==0){
+//         ans = nums[i]
+//         count=1
+//     }else if(nums[i]===ans){
+//         count++
+//     }else{
+//         count--
+//     }
+// }
+// return ans
+// }
+
+// let res = majority([5,5,1,1,1,5,5])
+// console.log(res);
+
+
+// function sortcolor(nums){
+// let i=0,k=0,j=nums.length-1
+// while(k<=j){
+//     if(nums[k]==0){
+//         let temp = nums[k]
+//         nums[k]=nums[i]
+//         nums[i]=temp
+//         i++
+//         k++
+//     }else if(nums[k]==2){
+//          let temp = nums[k]
+//         nums[k]=nums[j]
+//         nums[j]=temp
+//         j--
+//         k++
+//     }else{
+//          k++
+// }
+//     }
    
+// return nums
+// }
+
+// let res = sortcolor([1,0,1,2])
+// console.log(res);
+
+
+function topKFrequent(nums,k){
+let map = new Map()
+if(nums.length==1){
+    return nums
 }
-return ans
+for(let i=0;i<nums.length;i++){
+    if(!map.has(nums[i])){
+        map.set(nums[i],1)
+    }else{
+        map.set(nums[i],map.get(nums[i])+1)
+    }
 }
 
-let res = majority([5,5,1,1,1,5,5])
+let arr =  Array.from(map.entries())
+arr.sort((a,b)=>b[1]-a[1])
+let result = []
+for(let j=0;j<k;j++){
+    result.push(arr[j][0])
+}
+return result
+}
+let res = topKFrequent([1,2,2,3,3,3],2)
 console.log(res);
