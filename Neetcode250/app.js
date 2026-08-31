086c39c3ed01cdb87f296f4a175b037d5ed1dc19
Sudeep <sudeepsudee828@gmail.com>
2026-08-31 21:43:38 +0530

    groupAnagrams

--- Neetcode250/app.js
+++ Neetcode250/app.js
@@ -10,7 +10,6 @@
 // let res =  twosum([3,4,5,6],7)
 // console.log(res);
 
-
 // function isAnagram(s,t){
 // if(s.length!==t.length){
 //     return false
@@ -26,8 +25,6 @@
 // }
 // let res = isAnagram("racecar","carrace")
 // console.log(res);
-
-
 
 // function getConcatenation(nums){
 // let ans = new Array(nums.length*2)
@@ -50,8 +47,6 @@
 // let res = getConcatenation([1,4,1,2])
 // console.log(res);
 
-
-
 // function hasDuplicate(nums){
 // let newArr = []
 // for(let i=0;i<nums.length;i++){
@@ -66,7 +61,6 @@
 
 // let res = hasDuplicate([1, 2, 3, 4])
 // console.log(res);
-
 
 // function  removeElement(nums,val){
 // let i=0
@@ -85,7 +79,6 @@
 // let res = removeElement([3,2,2,3],3)
 // console.log(res);
 
-
 // function longestCommonPrefix(strs){
 // let  prefix = strs[0]
 
@@ -94,7 +87,7 @@
 //     while(!strs[i].startsWith(prefix)){
 //         prefix = prefix.slice(0,-1)
 //         console.log(prefix);
-        
+
 //         if(prefix==""){
 //             return ""
 //         }
@@ -105,7 +98,6 @@
 
 // let res = longestCommonPrefix(["bat","bag","bank","band"])
 // console.log(res);
-
 
 // function  majorityElement(nums){
 //  let n = nums.length
@@ -119,24 +111,21 @@
 //     }
 // }
 
-
 // for(let j=0;j<n;j++){
 // let value = map.get(nums[j])
 // if(value>n/2){
 //     return nums[j]
 // }
-    
-    
+
 // }
 // }
 
 // let res =  majorityElement([2,2,1,1,1,2,2])
 // console.log(res);
 
-
 // function majority(nums){
 // let ans =  nums[0]
-// let count =1 
+// let count =1
 // for(let i=1;i<nums.length;i++){
 //     if(count==0){
 //         ans = nums[i]
@@ -152,7 +141,6 @@
 
 // let res = majority([5,5,1,1,1,5,5])
 // console.log(res);
-
 
 // function sortcolor(nums){
 // let i=0,k=0,j=nums.length-1
@@ -173,34 +161,50 @@
 //          k++
 // }
 //     }
-   
+
 // return nums
 // }
 
 // let res = sortcolor([1,0,1,2])
 // console.log(res);
 
+// function topKFrequent(nums,k){
+// let map = new Map()
+// if(nums.length==1){
+//     return nums
+// }
+// for(let i=0;i<nums.length;i++){
+//     if(!map.has(nums[i])){
+//         map.set(nums[i],1)
+//     }else{
+//         map.set(nums[i],map.get(nums[i])+1)
+//     }
+// }
 
-function topKFrequent(nums,k){
+// let arr =  Array.from(map.entries())
+// arr.sort((a,b)=>b[1]-a[1])
+// let result = []
+// for(let j=0;j<k;j++){
+//     result.push(arr[j][0])
+// }
+// return result
+// }
+// let res = topKFrequent([1,2,2,3,3,3],2)
+// console.log(res);
+
+function groupAnagrams(strs) {
 let map = new Map()
-if(nums.length==1){
-    return nums
-}
-for(let i=0;i<nums.length;i++){
-    if(!map.has(nums[i])){
-        map.set(nums[i],1)
-    }else{
-        map.set(nums[i],map.get(nums[i])+1)
-    }
-}
 
-let arr =  Array.from(map.entries())
-arr.sort((a,b)=>b[1]-a[1])
-let result = []
-for(let j=0;j<k;j++){
-    result.push(arr[j][0])
+for(let s of strs){
+  let sorted_S = s.split("").sort().join("")
+  console.log(sorted_S);
+  
+if(!map.has(sorted_S)){
+  map.set(sorted_S,[])
 }
-return result
+  map.get(sorted_S).push(s)
 }
-let res = topKFrequent([1,2,2,3,3,3],2)
-console.log(res);
+return Array.from(map.values())
+}
+let res = groupAnagrams(["act", "pots", "tops", "cat", "stop", "hat"]);
+console.log(res)
